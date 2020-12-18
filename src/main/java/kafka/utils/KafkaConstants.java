f26c0ac6fa5dba99037d0c19bca0f2bb6c501c09
Ketul <ptl.ketul@gmail.com>
2020-12-18 22:17:46 +0000

    kafka constants

---
+++ src/main/java/kafka/utils/KafkaConstants.java
@@ -0,0 +1,10 @@
+package kafka.utils;
+
+public class KafkaConstants {
+    public static final String BOOTSTRAP_SERVERS = "localhost:9092, localhost:9093, localhost:9094";
+    public static final String SINGLE_PARTITION_TOPIC_NAME = "first-topic";
+    public static final String MULTI_PARTITION_TOPIC_NAME = "multi-partition-topic";
+
+    private KafkaConstants(){
+    }
+}
